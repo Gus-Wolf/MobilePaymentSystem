ea742d206db5807686eb897afac0c36700c9da31
Gus-Wolf <agw5265@psu.edu>
2025-03-05 16:45:19 -0500

    Javadocs added to Transaction Management view

--- src/TransactionMangement/View/TransactionManagementView.java
+++ src/TransactionMangement/View/TransactionManagementView.java
@@ -1,6 +1,10 @@
 package TransactionMangement.View;
 
 public class TransactionManagementView {
+    /**
+     *
+     * @param transactionId is the transaction that is being shown to the user so they know what the special ID is
+     */
     public void displayTransactionDetails(int transactionId) {
         System.out.println("Stub: Displaying details for transaction ID " + transactionId);
     }
