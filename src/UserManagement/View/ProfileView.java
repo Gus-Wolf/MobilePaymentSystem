9a80e0be644e784590e4513539107f6c3dea014b
Gus-Wolf <agw5265@psu.edu>
2025-03-05 16:46:10 -0500

    Javadocs added to ProfileView.java

--- src/UserManagement/View/ProfileView.java
+++ src/UserManagement/View/ProfileView.java
@@ -1,6 +1,10 @@
 package UserManagement.View;
 
 public class ProfileView {
+
+    /**
+     * This will be a simple way for the user to view their profile
+     */
     public void showProfile() {
         System.out.println("Profile");
     }
