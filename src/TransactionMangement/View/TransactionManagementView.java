162928753d9c5757ea327a3752ccb192d7e772c4
Gus-Wolf <agw5265@psu.edu>
2025-03-05 16:32:07 -0500

    Transaction Managemnt view workings and updated

--- src/TransactionMangement/View/TransactionManagementView.java
+++ src/TransactionMangement/View/TransactionManagementView.java
@@ -1,4 +1,7 @@
 package TransactionMangement.View;
 
 public class TransactionManagementView {
+    public void displayTransactionDetails(int transactionId) {
+        System.out.println("Stub: Displaying details for transaction ID " + transactionId);
+    }
 }
