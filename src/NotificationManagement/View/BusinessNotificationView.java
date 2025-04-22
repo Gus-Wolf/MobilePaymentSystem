2c75d5af074ff383ac7bc19c5504fef92431dc9c
Ethan Snyder <ems7100@psu.edu>
2025-04-22 12:24:39 -0400

    Added GUI for notification

--- src/NotificationManagement/View/BusinessNotificationView.java
+++ src/NotificationManagement/View/BusinessNotificationView.java
@@ -2,26 +2,32 @@ package NotificationManagement.View;
 
 import NotificationManagement.Model.Notification;
 import NotificationManagement.Model.PaymentNotification;
+
+import javax.swing.*;
 import java.util.List;
 
 public class BusinessNotificationView implements NotificationViewInterface {
-    @Override
-    public void showNotification(String content) {
-        System.out.println("[BUSINESS ALERT] " + content);
+    private final DefaultListModel<String> notifications;
+
+    public BusinessNotificationView(DefaultListModel<String> notifications) {
+        this.notifications = notifications;
     }
 
-    // Displays a colorful dashboard of notifications
     @Override
-    public void displayNotifications(List<Notification> notifications) {
-        System.out.println("\n=== BUSINESS DASHBOARD ===");
-        for(int i = 0; i < notifications.size(); i++) {
-            Notification n = notifications.get(i);
-            String color = (i % 2 == 0) ? "WHITE" : "LIGHT_GRAY";
+    public void showNotification(String content) {
+        notifications.addElement("[BUSINESS ALERT] " + content);
+    }
 
-            if(n instanceof PaymentNotification) {
-                System.out.println("\u001B[34m" + n.getContent() + " (" + color + " ROW)\u001B[0m");
+    @Override
+    public void displayNotifications(List<Notification> notificationsList) {
+        notifications.clear();
+        for (int i = 0; i < notificationsList.size(); i++) {
+            Notification n = notificationsList.get(i);
+            String color = (i % 2 == 0) ? "WHITE" : "LIGHT_GRAY";
+            if (n instanceof PaymentNotification) {
+                notifications.addElement("[PAYMENT] " + n.getContent() + " (" + color + " ROW)");
             } else {
-                System.out.println(n.getContent() + " (" + color + " ROW)");
+                notifications.addElement(n.getContent() + " (" + color + " ROW)");
             }
         }
     }
